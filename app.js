900ae118ad1edf05ece95efda1dabd71e42ab3bf
jayak <kumarijaya127@gmail.com>
2024-06-08 17:12:24 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const path = require("path");
+const crypto = require("crypto");
+const multer = require("multer");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/images/uploads");
+  },
+  filename: function (req, file, cb) {
+    crypto.randomBytes(12, (err, bytes) => {
+      const fn = bytes.toString("hex") + path.extname(file.originalname);
+      cb(null, fn);
+    });
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/test", (req, res) => {
+  res.render("test");
+});
+
+app.post("/upload", upload.single("image"), (req, res) => {
+  console.log(req.file);
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`listening to port ${port}...`);
+});
