a02b269981282021851e7a9e9f2a011e981348ba
jayak <kumarijaya127@gmail.com>
2024-06-09 21:38:39 +0530

    added multer

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
 const express = require("express");
 const path = require("path");
 const upload = require("./config/multer-config");
-
+const userModel = require("./models/user");
+const cors = require("cors");
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
-
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "./public/images/uploads");
-//   },
-//   filename: function (req, file, cb) {
-//     crypto.randomBytes(12, (err, bytes) => {
-//       const fn = bytes.toString("hex") + path.extname(file.originalname);
-//       cb(null, fn);
-//     });
-//   },
-// });
-
-// const upload = multer({ storage: storage });
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -31,20 +19,17 @@ app.get("/test", (req, res) => {
   res.render("test");
 });
 
-app.get("/profile", (req, res) => {
+app.get("/profile", async (req, res) => {
   res.render("profile");
 });
 
-app.get("/profile/uploads", (req, res) => {
+app.get("/profile/upload", (req, res) => {
   res.render("test");
 });
 
-// app.post("/uploads", (req, res) => {
-//   res.render("test");
-// });
-
-app.post("/upload", upload.single("image"), (req, res) => {
-  console.log(req.file);
+app.post("/upload", upload.single("image"), async (req, res) => {
+  await user.save();
+  res.redirect("/profile");
 });
 
 const port = 3000;
--- models/user.js
+++ models/user.js
@@ -3,13 +3,7 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/fileuploads");
 
 const userSchema = mongoose.Schema({
-  username: String,
-  email: String,
-  password: String,
-  profilePic: {
-    type: String,
-    default: "default.png",
-  },
+  image: String,
 });
 
 module.exports = mongoose.model("user", userSchema);
